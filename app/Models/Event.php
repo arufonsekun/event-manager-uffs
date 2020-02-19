6b5e77d8ecd01935989b0312fb7423fb2f8a8c15
arufonsekun <junior.ramisch@gmail.com>
2020-02-18 23:51:00 -0300

    Creates Event model, controller resource and migration

---
+++ app/Models/Event.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Event extends Model
+{
+
+    use SoftDeletes;
+    
+    protected $table = 'events';
+
+    protected $fillable = [
+        'name',
+        'date',
+        'description',
+        'place'
+    ];
+
+    protected $casts = [
+        'sections' => 'array'
+    ];
+}
